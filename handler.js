b8c13e9b12e9cfef3d536d0399a12ed75db2d05a
Mark Marijnissen <markmarijnissen@gmail.com>
2018-08-30 15:42:46 +0200

    update dependencies and write more comments in handler.js

--- handler.js
+++ handler.js
@@ -2,16 +2,16 @@
 const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
 const toJsonApi = require("@orikami/fn-to-json-api");
 const auth0 = require("@orikami/micro-auth0")(require("./auth0"));
+const tryCatch = require("@orikami/micro-trycatch");
 const cors = require("micro-cors")();
-const tryCatch = require("../micro-trycatch");
 const toLambda = require("@orikami/micro-to-lambda");
 
 const handler = compose(
-  tryCatch,
-  cors,
-  auth0,
-  toJsonApi
-)(require("./index"));
+  tryCatch,     // Catch errors, format as JSON
+  cors,         // Send correct CORS headers
+  auth0,        // Enable Auth0 authentiation, see auth0.js for config
+  toJsonApi     // Convert (data)=>{} to (req,res)=>{} for micro
+)(require("./index")); // Business logic: (data)=>{}
 
 // Default export: the micro function handler
 module.exports = handler;
@@ -20,8 +20,8 @@ module.exports = handler;
 module.exports.lambda = toLambda(handler);
 
 // Run in bash
-if(require.main === module) {
-    const micro = require("micro");
-    const server = micro(handler);
-    server.listen(process.env.PORT || 3000);
+if (require.main === module) {
+  const micro = require("micro");
+  const server = micro(handler);
+  server.listen(process.env.PORT || 3000);
 }
