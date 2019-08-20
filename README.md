52602cebb946c0dc7f378f2c35d5675172801a49
Mark Marijnissen <markmarijnissen@gmail.com>
2019-08-20 13:34:33 +0200

    upgrade boilerplate dependencies, remove aws lambda

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# __NAME__
+# **NAME**
 
-This is the amazing __NAME__ microservice.
+This is the amazing **NAME** microservice.
 
 ## Install
+
 Required:
+
 - node > 8 and NPM
 - serverless (`npm install -g serverless`)
 
@@ -12,21 +14,21 @@ npm install
 ```
 
 ## Develop
+
 ```bash
 npm run start  # micro will take handler.js and spin up a server
 npm run dev    # development server watches files
 ```
 
-## Test 
+## Test
+
 ```bash
 npm run test -- --watchAll
 ```
 
 ## Deploy
-```bash
-# first time, deploy entire stack
-serverless deploy --stage=staging
 
-# second time, only update function code
-serverless deploy --function __NAME__ --stage=staging
+```bash
+# Deploy with orikami-cli and gitops
+ok k8s deploy
 ```
--- src/handler.js
+++ src/handler.js
@@ -4,20 +4,16 @@ const toJsonApi = require("@orikami/fn-to-json-api");
 const auth0 = require("@orikami/micro-auth0")(require("./auth0"));
 const tryCatch = require("@orikami/micro-trycatch");
 const cors = require("micro-cors")();
-const toLambda = require("@orikami/micro-to-lambda");
 
 const handler = compose(
-  tryCatch,     // Catch errors, format as JSON
-  cors,         // Send correct CORS headers
-  auth0,        // Enable Auth0 authentiation, see auth0.js for config
-  toJsonApi     // Convert (data)=>{} to (req,res)=>{} for micro
+  tryCatch, // Catch errors, format as JSON
+  cors, // Send correct CORS headers
+  auth0, // Enable Auth0 authentiation, see auth0.js for config
+  toJsonApi // Convert (data)=>{} to (req,res)=>{} for micro
 )(require("./index")); // Business logic: (data)=>{}
 
 // Default export: the micro function handler
 module.exports = handler;
-
-// Lambda export: the lambda function handler (serverless.yml)
-module.exports.lambda = toLambda(handler);
 
 // Run in bash
 if (require.main === module) {
