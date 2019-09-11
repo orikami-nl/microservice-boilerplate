8e5b56f279062a6414d3d8e89297900d0262725c
StijnAvdL <stijn@orikami.nl>
2019-09-11 11:11:34 +0200

    add router to boilerplate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
+#!/usr/bin/env node
 require("dotenv").config({ path: ".env-" + (process.env.STAGE || "staging") });
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
+const toJsonApi = require("@orikami/fn-to-json-api");
+const auth0 = require("@orikami/micro-auth0")(require(`${process.env.AUTH0_CONFIG}`));
+const tryCatch = require("@orikami/micro-trycatch");
+const cors = require("micro-cors")();
+const { send } = require('micro');
+let match = require('fs-router')(__dirname + '/api');
 
-// 'data' contains query + POST data
-const handler = async data => {
-  // Errors with 4xx of 5xx will set the HTTP status code
-  // throw new Error("404 Could not find the document");
-  
-  return {  greeting: `Hello __NAME__`, data };
+const router = async function(req, res) {
+  let matched = toJsonApi(match(req));
+  if (matched) return await matched(req, res);
+  send(res, 404, { error: 'Method not found' })
 };
 
+const handler = compose(
+  tryCatch, // Catch errors, format as JSON
+  cors, // Send correct CORS headers
+  auth0, // Enable Auth0 authentiation, see auth0.js for config
+)(router); // Business logic: (data)=>{}
+
+// Run in bash
+if (require.main === module) {
+  const micro = require("micro");
+  const server = micro(handler);
+  server.listen(process.env.PORT || 3000);
+}
+
+// Default export: the micro function handler
 module.exports = handler;
